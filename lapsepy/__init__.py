ade54b22fbd0c9578c6424e5871522e15f8909c7
Quintin Dunn <93884113+quintindunn@users.noreply.github.com>
2023-10-22 14:34:14 -0400

    Update version

--- lapsepy/__init__.py
+++ lapsepy/__init__.py
@@ -3,6 +3,6 @@ Author: Quintin Dunn
 Date: 10/22/23
 """
 
-__version__ = '0.0.1'
+__version__ = '0.0.1.2'
 
 from .journal import Journal
--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from setuptools import setup, find_packages
 
-VERSION = "0.0.1.1"
+from lapsepy import __version__
+
+VERSION = __version__
 DESCRIPTION = "A Python API wrapper for the social media app Lapse."
 LONG_DESCRIPTION = "An unofficial API wrapper for the social media app Lapse."
 
