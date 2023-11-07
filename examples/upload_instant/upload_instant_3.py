62cc1c79e07bd3e78345366da261c7a951e9df55
Quintin Dunn <93884113+quintindunn@users.noreply.github.com>
2023-11-07 15:27:21 -0500

    Updated comments in upload_instant example

--- examples/upload_instant/upload_instant_3.py
+++ examples/upload_instant/upload_instant_3.py
@@ -6,7 +6,6 @@ if __name__ == '__main__':
     lapse = Lapse(refresh_token=os.getenv("REFRESH_TOKEN"))
     upload_im = Image.open("../imgs/example_1.jpg")
 
-    # Develop in 15 seconds
     friend_id = input("Friend UUID: ")
 
     # Get friend object
